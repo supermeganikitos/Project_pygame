fe9b787376e84dc1b06e2345ab3034ba42bd3e57
supermeganikitos <nik777555123@gmail.ru>
2023-12-21 20:06:59 +0400

    чуть не стер

--- mane.py
+++ mane.py
@@ -1,48 +1,9 @@
 import pygame
-import os
-import sys
+from trucks import Truck, trucks
 
 pygame.init()
 size = width, height = 1550, 800
 screen = pygame.display.set_mode(size)
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
-
-class Truck(pygame.sprite.Sprite):
-    image = load_image("truck.png", -1)
-
-    def __init__(self, x, y, *group):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
-        # Это очень важно!!!
-        super().__init__(*group)
-        self.image = Truck.image
-        self.rect = self.image.get_rect()
-        self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = x
-        self.rect.y = y
-        self.vx = 1
-        self.vy = 0
-        self.add(trucks)
-
-    def update(self):
-        if self.rect.x > width:
-            self.rect.x = self.image.get_size()[0] * -1
-        self.rect = self.rect.move(self.vx, self.vy)
 
 
 def draw_(screen, n):
@@ -57,7 +18,6 @@ def draw_(screen, n):
                                            text_w + 20, text_h + 20), 10)
 
 
-trucks = pygame.sprite.Group()
 pygame.display.set_caption('simulator truck')
 pygame.display.flip()
 running = True
@@ -66,8 +26,6 @@ y_step = 90
 for x in range(0, width, x_step):
     for y in range(0, height, y_step):
         Truck(x, y, trucks)
-
-
 clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
@@ -78,6 +36,4 @@ while running:
     trucks.draw(screen)
     draw_(screen, 'Truck simulator')
     clock.tick(10)
-
-
     pygame.display.flip()
--- trucks.py
+++ trucks.py
@@ -1,12 +1,68 @@
 import pygame
+import os
+import sys
 
-all_sprites = pygame.sprite.Group()
+trucks = pygame.sprite.Group()
 
 
-class SimpleButton(pygame.sprite.Sprite): # от него будем наследовать все кнопки и destination тоже
-    pass
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
-class Truck:
+
+class Truck(pygame.sprite.Sprite):
+    image = load_image("truck.png", -1)
+
+    def __init__(self, x, y, *group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно!!!
+        super().__init__(*group)
+        self.image = Truck.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = x
+        self.rect.y = y
+        self.vx = 1
+        self.vy = 0
+        self.add(trucks)
+
+    def update(self):
+        if self.rect.x > 1550:
+            self.rect.x = self.image.get_size()[0] * -1
+        self.rect = self.rect.move(self.vx, self.vy)
+
+
+class SimpleButton(pygame.sprite.Sprite):  # от него будем наследовать все кнопки и destination тоже
+    def __init__(self, group, x, y, size_x=10, size_y=10, color=(200, 200, 200)):
+        super().__init__(*group)
+        self.image = pygame.Surface((size_x, size_y), pygame.SRCALPHA, 32)
+
+        pygame.draw.rect(self.image, pygame.Color(color),
+                         (0, 0, size_x, size_y))
+        self.rect = pygame.Rect(x, y, size_x, size_y)
+
+
+class MaskButton(SimpleButton):  # от него будем наследовать все кнопки и destination тоже
+    def __init__(self, group, x, y, img_name):
+        super().__init__(group, x, y, size_x=10, size_y=10, color=(0, 0, 0))
+        self.image = load_image(img_name)
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+class Truck1:
     def __init__(self):
         pass
 
@@ -14,12 +70,7 @@ class Truck:
         pass
 
 
-class Destination(pygame.sprite.Sprite): # Ты наследовал спрайт от группы спрайтов я это исправил
-    def __init__(self):
-        super().__init__(all_sprites)
 
-    def what_check_in_this(self):
-        self.dict_of_changes = [] # словарь где по ключу (части авто) будут находится изменения
 
 
 
